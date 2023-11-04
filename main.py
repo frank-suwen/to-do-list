8fe8a120dd83df4bba576f99ef3ff45ca937f937
frank-wsw <suwenw2@outlook.com>
2023-11-03 22:58:42 -0700

    add basic layout

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    tasks = []
+
+    while True:
+        print("\n===== To-Do List ======")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Mark Task as Done")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
+
+
+
+main()
